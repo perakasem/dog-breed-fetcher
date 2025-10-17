9ce8a603c6a1098e505755cc6f7b84dc0fbb6fdc
perakasem <pkasemsripitak@gmail.com>
2025-10-16 22:48:33 -0400

    admin

--- src/main/java/dogapi/BreedFetcher.java
+++ src/main/java/dogapi/BreedFetcher.java
@@ -21,8 +21,5 @@ public interface BreedFetcher {
         public BreedNotFoundException(String breed) {
             super("Breed not found: " + breed);
         }
-        public BreedNotFoundException(String breed, Throwable cause) {
-            super("Breed not found: " + breed, cause);
-        }
     }
 }
